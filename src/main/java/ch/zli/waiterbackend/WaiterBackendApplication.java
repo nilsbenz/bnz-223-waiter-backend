13b0a9c262c9aa6338ed940cab9871d2a65e8820
Nils Benz <nils.benz@adcubum.com>
2019-11-04 15:04:52 +0100

    init

---
+++ src/main/java/ch/zli/waiterbackend/WaiterBackendApplication.java
@@ -0,0 +1,13 @@
+package ch.zli.waiterbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaiterBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaiterBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/zli/waiterbackend/WaiterBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.zli.waiterbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaiterBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
